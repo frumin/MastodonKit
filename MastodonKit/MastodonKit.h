70c73ab75869d3276107ec567f20a63553ebd5a7
Tom Zaworowski <tom@acidbits.com>
2018-08-18 21:03:55 -0700

    Initial Commit

---
+++ MastodonKit/MastodonKit.h
@@ -0,0 +1,19 @@
+//
+//  MastodonKit.h
+//  MastodonKit
+//
+//  Created by Tom Zaworowski on 8/18/18.
+//  Copyright © 2018 Acidbits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MastodonKit.
+FOUNDATION_EXPORT double MastodonKitVersionNumber;
+
+//! Project version string for MastodonKit.
+FOUNDATION_EXPORT const unsigned char MastodonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MastodonKit/PublicHeader.h>
+
+
